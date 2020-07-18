d18be59d7c3ba727f67483e11df801d17908500a
Vitor B Appolinario <vitorappolinario@gmail.com>
2020-07-18 20:53:22 -0300

    Adiciona o mecanismo de persistencia do Orleans
    Esta utilizando o MariaDb como base de dados, tem que seguir os passos
    da documentação de onde peguei os os script MySQL-Main.sql e
    MySQL-Persistence.sql
    
    https://dotnet.github.io/orleans/Documentation/clusters_and_clients/configuration_guide/adonet_configuration.html

--- src/Grains/CartGrain.cs
+++ src/Grains/CartGrain.cs
@@ -4,44 +4,46 @@ using System.Threading.Tasks;
 using GrainInterfaces;
 using GrainInterfaces.States;
 using Orleans;
+using Orleans.Providers;
 
 namespace Grains
 {
+    [StorageProvider(ProviderName = "CartStorage")]
     public class CartGrain : Grain<CartState>, ICartGrain
     {
         public async Task AddProduct(Product product)
         {
-          await ReadStateAsync();
+            await ReadStateAsync();
 
-          if (State.State == null)
-            State = new CartState();
+            if (State.State == null)
+                State = new CartState();
 
-          State.State.Products.Add(product);
-          await WriteStateAsync();
+            State.State.Products.Add(product);
+            await WriteStateAsync();
         }
 
         public async Task<Cart> GetCart()
         {
-          await ReadStateAsync();
+            await ReadStateAsync();
 
-          if (State.State != null)
+            if (State.State != null)
+                return State.State;
+
+            State.State = new Cart
+            {
+                Id = Guid.NewGuid(),
+                Products = new List<Product>()
+            };
+
+            await WriteStateAsync();
+
             return State.State;
-
-          State.State = new Cart
-          {
-            Id = Guid.NewGuid(),
-               Products = new List<Product>()
-          };
-
-          await WriteStateAsync();
-
-          return State.State;
         }
 
         public async Task<List<Product>> GetProducts()
         {
-          await ReadStateAsync();
-          return State.State.Products;
+            await ReadStateAsync();
+            return State.State.Products;
         }
     }
 }
--- src/Silo/Program.cs
+++ src/Silo/Program.cs
@@ -11,7 +11,13 @@ namespace Silo
         static async Task Main(string[] args)
         {
             var siloBuilder = new SiloHostBuilder()
-                .UseLocalhostClustering(serviceId:"orleans-cartapi")
+                .UseLocalhostClustering(serviceId: "orleans-cartapi")
+                .AddAdoNetGrainStorage("CartStorage", options =>
+                    {
+                        options.Invariant = "MySql.Data.MySqlClient";
+                        options.ConnectionString = "host=127.0.0.1;port=3306;user id=root;password=admin;database=CartApi";
+                        options.UseJsonFormat = true;
+                    })
                 .ConfigureLogging(logging => logging.AddConsole());
 
             using (var host = siloBuilder.Build())
