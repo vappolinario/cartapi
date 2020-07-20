aa347bc520ee97e9369f66a4099ebf0c21cdbfc9
Vitor B Appolinario <vitorappolinario@gmail.com>
2020-07-19 21:58:29 -0300

    Reduz o tamanho da primeira migration

--- sql/01-Migration.sql
+++ sql/01-Migration.sql
@@ -1,51 +1,6 @@
 DROP DATABASE IF EXISTS `CartApi`;
 CREATE DATABASE CartApi;
 USE CartApi;
--- The design criteria for this table are:
---
--- 1. It can contain arbitrary content serialized as binary, XML or JSON. These formats
--- are supported to allow one to take advantage of in-storage processing capabilities for
--- these types if required. This should not incur extra cost on storage.
---
--- 2. The table design should scale with the idea of tens or hundreds (or even more) types
--- of grains that may operate with even hundreds of thousands of grain IDs within each
--- type of a grain.
---
--- 3. The table and its associated operations should remain stable. There should not be
--- structural reason for unexpected delays in operations. It should be possible to also
--- insert data reasonably fast without resource contention.
---
--- 4. For reasons in 2. and 3., the index should be as narrow as possible so it fits well in
--- memory and should it require maintenance, isn't resource intensive. For this
--- reason the index is narrow by design (ideally non-clustered). Currently the entity
--- is recognized in the storage by the grain type and its ID, which are unique in Orleans silo.
--- The ID is the grain ID bytes (if string type UTF-8 bytes) and possible extension key as UTF-8
--- bytes concatenated with the ID and then hashed.
---
--- Reason for hashing: Database engines usually limit the length of the column sizes, which
--- would artificially limit the length of IDs or types. Even when within limitations, the
--- index would be thick and consume more memory.
---
--- In the current setup the ID and the type are hashed into two INT type instances, which
--- are made a compound index. When there are no collisions, the index can quickly locate
--- the unique row. Along with the hashed index values, the NVARCHAR(nnn) values are also
--- stored and they are used to prune hash collisions down to only one result row.
---
--- 5. The design leads to duplication in the storage. It is reasonable to assume there will
--- a low number of services with a given service ID operational at any given time. Or that
--- compared to the number of grain IDs, there are a fairly low number of different types of
--- grain. The catch is that were these data separated to another table, it would make INSERT
--- and UPDATE operations complicated and would require joins, temporary variables and additional
--- indexes or some combinations of them to make it work. It looks like fitting strategy
--- could be to use table compression.
---
--- 6. For the aforementioned reasons, grain state DELETE will set NULL to the data fields
--- and updates the Version number normally. This should alleviate the need for index or
--- statistics maintenance with the loss of some bytes of storage space. The table can be scrubbed
--- in a separate maintenance operation.
---
--- 7. In the storage operations queries the columns need to be in the exact same order
--- since the storage table operations support optionally streaming.
 DROP TABLE IF EXISTS `OrleansQuery`;
 DROP TABLE IF EXISTS `OrleansStorage`;
 DROP PROCEDURE IF EXISTS `ClearStorage`;
@@ -61,15 +16,6 @@ CREATE TABLE OrleansQuery
 
 CREATE TABLE OrleansStorage
 (
-    -- These are for the book keeping. Orleans calculates
-    -- these hashes (see RelationalStorageProvide implementation),
-    -- which are signed 32 bit integers mapped to the *Hash fields.
-    -- The mapping is done in the code. The
-    -- *String columns contain the corresponding clear name fields.
-    --
-    -- If there are duplicates, they are resolved by using GrainIdN0,
-    -- GrainIdN1, GrainIdExtensionString and GrainTypeString fields.
-    -- It is assumed these would be rarely needed.
     GrainIdHash                INT NOT NULL,
     GrainIdN0                BIGINT NOT NULL,
     GrainIdN1                BIGINT NOT NULL,
@@ -78,33 +24,17 @@ CREATE TABLE OrleansStorage
     GrainIdExtensionString    NVARCHAR(512) NULL,
     ServiceId                NVARCHAR(150) NOT NULL,
 
-    -- The usage of the Payload records is exclusive in that
-    -- only one should be populated at any given time and two others
-    -- are NULL. The types are separated to advantage on special
-    -- processing capabilities present on database engines (not all might
-    -- have both JSON and XML types.
-    --
-    -- One is free to alter the size of these fields.
     PayloadBinary    BLOB NULL,
     PayloadXml        LONGTEXT NULL,
     PayloadJson        LONGTEXT NULL,
 
-    -- Informational field, no other use.
     ModifiedOn DATETIME NOT NULL,
 
-    -- The version of the stored payload.
     Version INT NULL
 
-    -- The following would in principle be the primary key, but it would be too thick
-    -- to be indexed, so the values are hashed and only collisions will be solved
-    -- by using the fields. That is, after the indexed queries have pinpointed the right
-    -- rows down to [0, n] relevant ones, n being the number of collided value pairs.
 ) ROW_FORMAT = COMPRESSED KEY_BLOCK_SIZE = 16;
 ALTER TABLE OrleansStorage ADD INDEX IX_OrleansStorage (GrainIdHash, GrainTypeHash);
 
--- The following alters the column to JSON format if MySQL is at least of version 5.7.8.
--- See more at https://dev.mysql.com/doc/refman/5.7/en/json.html for JSON and
--- http://dev.mysql.com/doc/refman/5.7/en/comments.html for the syntax.
 /*!50708 ALTER TABLE OrleansStorage MODIFY COLUMN PayloadJson JSON */;
 
 DELIMITER $$
@@ -127,7 +57,6 @@ BEGIN
 
     SET _newGrainStateVersion = _GrainStateVersion;
 
-    -- Default level is REPEATABLE READ and may cause Gap Lock issues
     SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
     START TRANSACTION;
     UPDATE OrleansStorage
@@ -179,28 +108,9 @@ BEGIN
 
     SET _newGrainStateVersion = _GrainStateVersion;
 
-    -- Default level is REPEATABLE READ and may cause Gap Lock issues
     SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
     START TRANSACTION;
 
-    -- Grain state is not null, so the state must have been read from the storage before.
-    -- Let's try to update it.
-    --
-    -- When Orleans is running in normal, non-split state, there will
-    -- be only one grain with the given ID and type combination only. This
-    -- grain saves states mostly serially if Orleans guarantees are upheld. Even
-    -- if not, the updates should work correctly due to version number.
-    --
-    -- In split brain situations there can be a situation where there are two or more
-    -- grains with the given ID and type combination. When they try to INSERT
-    -- concurrently, the table needs to be locked pessimistically before one of
-    -- the grains gets @GrainStateVersion = 1 in return and the other grains will fail
-    -- to update storage. The following arrangement is made to reduce locking in normal operation.
-    --
-    -- If the version number explicitly returned is still the same, Orleans interprets it so the update did not succeed
-    -- and throws an InconsistentStateException.
-    --
-    -- See further information at https://dotnet.github.io/orleans/Documentation/Core-Features/Grain-Persistence.html.
     IF _GrainStateVersion IS NOT NULL
     THEN
         UPDATE OrleansStorage
@@ -228,8 +138,6 @@ BEGIN
         END IF;
     END IF;
 
-    -- The grain state has not been read. The following locks rather pessimistically
-    -- to ensure only on INSERT succeeds.
     IF _GrainStateVersion IS NULL
     THEN
         INSERT INTO OrleansStorage
@@ -262,7 +170,6 @@ BEGIN
             1) AS TMP
         WHERE NOT EXISTS
         (
-            -- There should not be any version of this grain state.
             SELECT 1
             FROM OrleansStorage
             WHERE
